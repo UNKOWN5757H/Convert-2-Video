cd9e6ac9de14d0f70acd3684e9a0f86d3fc46ec3
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2020-06-17 18:07:05 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,7 +5,7 @@ You can convert any Telegram Document, using this bot! With custom thumbnail
 
 Create Legend Person : <a href='https://t.me/NGYNY'>😎 N I K H I L 😎</a>
     
-    SUPPORT Channel : <a href='https://t.me/ALL_MOVIES_LIABRARY'>ᏗᏝᏝ ᎷᎧᏉᎥᏋᏕ ᏝᎥᏰᏒᏗᏒᎩ</a>
+    SUPPORT Channel : <a href='https://t.me/NGY_BOTS'>NGY BOT'S</a>
     
     
     
@@ -16,25 +16,25 @@ Create Legend Person : <a href='https://t.me/NGYNY'>😎 N I K H I L 😎</a>
     DOWNLOAD_START = "trying to download"
     UPLOAD_START = "trying to upload"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/ALL_MOVIES_LIABRARY"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "THANKS FOR USING ME , JOIN OUR CHANNEL : <a href='https://t.me/NGY_BOTS'>NGY_BOT'S</a> "
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=RENAME_NGY_BOT-bot \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = "Join @ALL_MOVIES_LIABRARY...have a nyc day!"
+    CUSTOM_CAPTION_UL_FILE = "Join <a href='https://t.me/NGY_BOTS'>NGY BOT'S</a>...have a nyc day!"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     CURENT_PLAN_DETAILS = """Current plan details
 --------
 Telegram ID: <code>{}</code>
-Plan name: <a href='https://t.me/ALL_MOVIES_LIABRARY'>{}</a>
+Plan name: <a href='https://t.me/NGY_BOTS'>{}</a>
 Expires on: {}"""
     HELP_USER = """I can do:
-👉 <a href="https://t.me/ALL_MOVIES_LIABRARY">JOIN OUR CHANNEL</a>
+👉 <a href="https://t.me/NGY_BOTS">JOIN OUR CHANNEL</a>
 --------
 Send /me to know current plan details
 
-Support Channel : @ALL_MOVIES_LIABRARY"""
+Support Channel : @NGY_BOTS"""
 
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
